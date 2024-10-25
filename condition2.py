5d66e19da775abe75abe94a0fff4f0b3c4bc9142
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-25 14:32:19 +0530

    Add files via upload

---
+++ condition2.py
@@ -0,0 +1,8 @@
+'''Author: Anugrah Chandran
+Date: 25-10-24
+Description: Python program to find whether the number is positive or negative'''
+number=int(input("Enter your first number"))
+if number>0:
+    print(number," is positive ")
+else:
+    print(number," is negative ")
